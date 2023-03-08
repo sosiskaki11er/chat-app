98234142bf927a0796b46169f197ab18c2ef5c7f
amir <amikamaymun@gmail.com>
2023-03-08 16:55:47 +0500

    fix: deploy fix

--- client/src/ChatsPage.jsx
+++ client/src/ChatsPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
 
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic('63fc90b6-dde6-40c0-8b82-7c55d23c3324', props.user.username, props.user.secret)
   return (
     <div style={{height: '100vh'}}>
-        <MultiChatSocket {...chatProps}/>
-        <MultiChatWindow {...chatProps} style={{height: '100%'}} />
+        <PrettyChatWindow
+            projectId='63fc90b6-dde6-40c0-8b82-7c55d23c3324'
+            username={props.user.username}
+            secret={props.user.secret}
+            style={{ height: '100%' }}
+        />
     </div>
   )
 }
